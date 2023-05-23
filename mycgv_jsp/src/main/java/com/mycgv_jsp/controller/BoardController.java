c1658be50d4a1cf6533dc92f7203a0a98795d14a
kyj <willbackv@gmail.com>
2023-05-23 15:28:11 +0900

    commit_0523

--- mycgv_jsp/src/main/java/com/mycgv_jsp/controller/BoardController.java
+++ mycgv_jsp/src/main/java/com/mycgv_jsp/controller/BoardController.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.mycgv_jsp.dao.BoardDao;
 import com.mycgv_jsp.vo.BoardVo;
 
@@ -73,6 +77,73 @@ public class BoardController {
 		
 		return model;
 	} 
+	
+	
+	/** board_lsit_json_data.do - ajax에서 호출되는 게시글 전체 리스트_json **/
+	@RequestMapping(value="/board_list_json_data.do", method=RequestMethod.GET, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String board_list_json_data(String page) {
+		BoardDao boardDao = new BoardDao();
+		
+		//페이징 처리 - startCount, endCount 구하기
+				int startCount = 0;
+				int endCount = 0;
+				int pageSize = 5;	//한페이지당 게시물 수
+				int reqPage = 1;	//요청페이지	
+				int pageCount = 1;	//전체 페이지 수
+				int dbCount = boardDao.totalRowCount();	//DB에서 가져온 전체 행수
+				
+				//총 페이지 수 계산
+				if(dbCount % pageSize == 0){
+					pageCount = dbCount/pageSize;
+				}else{
+					pageCount = dbCount/pageSize+1;
+				}
+
+				//요청 페이지 계산
+				if(page != null){
+					reqPage = Integer.parseInt(page);
+					startCount = (reqPage-1) * pageSize+1; 
+					endCount = reqPage * pageSize;
+				}else{
+					startCount = 1;
+					endCount = pageSize;
+				}
+				
+				ArrayList<BoardVo> list = boardDao.select(startCount, endCount);
+				
+				// list 객체의 데이터를 JSON 형태로 생성
+				JsonObject jlist = new JsonObject();
+				JsonArray jarray = new JsonArray();
+				
+				for(BoardVo boardVo : list) {
+					JsonObject jobj = new JsonObject(); //{}
+					jobj.addProperty("rno", boardVo.getRno()); // {rno:1}
+					jobj.addProperty("btitle", boardVo.getBtitle()); //{rno:1, btitle:~~~}
+					jobj.addProperty("bhits", boardVo.getBhits());
+					jobj.addProperty("id", boardVo.getId());
+					jobj.addProperty("bdate", boardVo.getBdate());
+					
+					jarray.add(jobj);
+				}
+				
+				jlist.add("jlist", jarray); // = model.addObject("list", list); 
+				jlist.addProperty("totals", dbCount); // = model.addObject("totals", dbCount);
+				jlist.addProperty("pageSize", pageSize); // = model.addObject("pageSize", pageSize);
+				jlist.addProperty("maxSize", pageCount); // = model.addObject("maxSize", pageCount);
+				jlist.addProperty("page", reqPage); // = model.addObject("page", reqPage);
+				
+				/* System.out.println(jlist.toString()); */
+				
+				return new Gson().toJson(jlist);
+			} 
+	
+	
+	// header 게시판(JSON) 호출되는 주소
+	@RequestMapping(value="/board_list_json.do", method=RequestMethod.GET)
+	public String board_list_json() {
+		return "/board/board_list_json";
+	}
 	
 	
 	/** board_content.do - 게시글 상세 보기 **/
@@ -182,5 +253,5 @@ public class BoardController {
 		
 		return viewName;
 	}
-
+	
 } // class
